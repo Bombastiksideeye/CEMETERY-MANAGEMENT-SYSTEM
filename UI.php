37a9f77dae074a2661c9d8d6feb9bc46d861d281
ristine2004 <162429693+ristine2004@users.noreply.github.com>
2024-06-01 14:31:03 +0800

    Add files via upload

---
+++ UI.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submit Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/ecd5e25db3.js" crossorigin="anonymous"></script>
+</head>
+<body class=" bg-green-200">
+<div class=" navbar bg-base-100 fixed z-50">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </div>
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 ">
+        <li><a>Deceased Profile</a></li>
+        <li><a>Payment History</a></li>
+        <li><a>My Account</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center">
+    
+    <a class=" text-green-900 text-[30px] font-semibold font-serif">Compassion Memorial Gardens</a>
+  </div>
+  <div class="navbar-end mr-5 ">
+ 
+    
+<div class="dropdown dropdown-end">
+    <button onclick="document.getElementById('logout_modal').showModal()" class="w-full pl-5 pr-5 pb-2 pt-2 bg-green-700 text-white rounded-lg hover:bg-green-800">Log out</button>
+</div>
+
+
+
+               
+</body>
+</html>
+
+                
